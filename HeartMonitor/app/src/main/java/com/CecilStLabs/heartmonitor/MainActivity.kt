6a4f47d1d0b7a4cc6bca153b631f1a81204dd64f
Andrew Mueller <andrew.mueller@Turing25.local>
2026-06-19 21:11:23 -0500

    adding some example scanning code that isn't hooked up yet

--- HeartMonitor/app/src/main/java/com/CecilStLabs/heartmonitor/MainActivity.kt
+++ HeartMonitor/app/src/main/java/com/CecilStLabs/heartmonitor/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.CecilStLabs.heartmonitor
 
+import android.Manifest
+import android.util.Log
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,27 +22,134 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.CecilStLabs.heartmonitor.ui.theme.HeartMonitorTheme
 
+class HeartMonitorScanCallback : android.bluetooth.le.ScanCallback()
+{
+
+    override fun onScanResult(callbackType: Int, result: ScanResult?)
+    {
+        if (null != result) {
+            Log.d("HeartMonitor", "FOUND: " + result.scanRecord?.deviceName)
+        }
+    }
+
+    /**
+     * Callback when batch results are delivered.
+     *
+     * @param results List of scan results that are previously scanned.
+     */
+    override fun onBatchScanResults(results: MutableList<ScanResult?>?)
+    {
+        Log.d("HeartMonitor", "batch")
+    }
+
+    /**
+     * Callback when scan could not be started.
+     *
+     * @param errorCode Error code (one of SCAN_FAILED_*) for scan failure.
+     */
+    /*override fun onScanFailed(@ScanCallback.ScanFailed errorCode: Int)
+    {
+        Log.d("HeartMonitor", "FAILED: $errorCode")
+    }*/
+}
+
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    // Stops scanning after 10 seconds.
+    private val SCAN_PERIOD: Long = 10000
+
+    // true indicates the ble adapter is actively scanning
+    private var scanning = false
+
+    private val bluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().bluetoothLeScanner
+
+    private val handler = Handler()
+
+    //private val leDeviceListAdapter = LeDeviceListAdapter()
+
+    // Device scan callback.
+    private val leScanCallback: ScanCallback = object : ScanCallback()
+    {
+        override fun onScanResult(callbackType: Int, result: ScanResult)
+        {
+            super.onScanResult(callbackType, result)
+
+            Log.d("HeartMonitor", "Found " + result.device)
+
+            // i have no idea what this is
+            //leDeviceListAdapter.addDevice(result.device)
+            //leDeviceListAdapter.notifyDataSetChanged()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            HeartMonitorTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+
+
+        val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+        val bluetoothAdapter: BluetoothAdapter? = bluetoothManager.getAdapter()
+
+        if (bluetoothAdapter == null) {
+
+            setContent {
+                HeartMonitorTheme {
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        Greeting(
+                            name = "Device doesn't support Bluetooth",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
+                }
+            }
+        }
+        else
+        {
+            setContent {
+                HeartMonitorTheme {
+                    Scaffold(modifier = Modifier.fillMaxSize())
+                    { innerPadding ->
+                        Greeting(
+                            name = "Scanning ...",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
     }
+
+    @RequiresPermission(Manifest.permission.BLUETOOTH_SCAN)
+    private fun scanLeDevice()
+    {
+        if (!scanning)
+        {
+            // Stops scanning after a pre-defined scan period.
+            handler.postDelayed({
+                   scanning = false
+                   bluetoothLeScanner.stopScan(leScanCallback)
+            }, SCAN_PERIOD)
+
+            Log.d("HeartMonitor", "starting scanning...")
+            scanning = true
+            bluetoothLeScanner.startScan(leScanCallback)
+        }
+        else
+        {
+            Log.d("HeartMonitor", "starting scanning...")
+            scanning = false
+            bluetoothLeScanner.stopScan(leScanCallback)
+        }
+    }
+
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "$name",
         modifier = modifier
     )
 }
@@ -42,6 +158,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     HeartMonitorTheme {
-        Greeting("Android")
+        Greeting("")
     }
 }
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CecilSt-HeartMonitor
 Android Application for my cheapo bike heart monitor
+
+Exploring Android, Kotlin, and BLE
+
